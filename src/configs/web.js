f04191bbe1b642096134a1a45de42d7535ff1ca9
tungleqb <tungleqb@gmail.com>
2024-03-19 23:20:26 +0700

    MVC tutorial

--- src/configs/web.js
+++ src/configs/web.js
@@ -1,15 +1,13 @@
 import express from "express";
-
+import homeController from '../controller/homeController'
 const router = express.Router();
 
 /**
- * 
  * @param {*} app : express app
  */
 const initWebRoutes = (app) => {
-    router.get("/", (req, res) => {
-        return res.send("Hello world");
-    })
+    router.get("/", homeController.handleHelloWorld);
+    router.get("/user", homeController.handleUserPage);
     return app.use("/", router);
 }
 export default initWebRoutes;
